05e452c95701ba5130e7e240890359443c5d45ce
pipoblete <127996548+pipoblete@users.noreply.github.com>
2023-08-21 22:31:13 -0400

    cambios

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController, IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-login',
@@ -7,18 +9,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
+  @ViewChild(IonModal) modal!: IonModal;
+
   email: string = '';
   password: string = '';
+  message: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private alertController: AlertController) {}
 
-  login() {
-    
+  async login() {
     if (this.email === 'milo@example.com' && this.password === 'contraseña') {
       this.router.navigate(['/home']); 
+      this.modal.dismiss(this.email, 'confirm');
     } else {
-      console.log('Credenciales incorrectas');
+      this.modal.dismiss(null, 'cancel');
+      await this.presentAlert('Credenciales incorrectas');
+      
+    }
+  }
+
+  async presentAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Error de autenticación',
+      message: 'Ingrese nuevamente sus datos',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
     }
   }
 }
-
