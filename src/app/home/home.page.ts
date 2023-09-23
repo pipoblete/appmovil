a22d68b8360c8e3173b8d70bace7f18921ed7f7b
pipoblete <127996548+pipoblete@users.noreply.github.com>
2023-09-23 19:38:13 -0300

    se actualizó el ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,54 +1,55 @@
-import { Component, Renderer2, ElementRef} from '@angular/core';
+import { Component, Renderer2, ElementRef } from '@angular/core';
 import { UserService } from '../user.service';
 import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
+  loggedInUser: string = '';
+  name: string = '';
+  lastname: string = '';
+  educationLevel: string = '';
+  fecha: string = '';
+  isAnimated: boolean = false;
+  segmentValue: string = 'default'; // Valor predeterminado para el segmento
 
-    loggedInUser: string = '';
-    name: string = '';
-    lastname: string = '';
-    educationLevel: string = '';
-    fecha: string = '';
-    isAnimated: boolean = false;
+  constructor(
+    private userService: UserService,
+    private alertController: AlertController,
+    private renderer: Renderer2,
+    private el: ElementRef
+  ) {}
 
+  ngOnInit() {
+    this.loggedInUser = this.userService.getLoggedInUser();
+  }
 
-    constructor(private userService: UserService, private alertController: AlertController, private renderer:  Renderer2, private el: ElementRef) {}
+  limpiarCampos() {
+    this.name = '';
+    this.lastname = '';
+    this.educationLevel = '';
+    this.isAnimated = true;
 
-    ;
-  
+    setTimeout(() => {
+      this.isAnimated = false;
+    }, 1000);
+  }
 
-
-    ngOnInit() {
-      this.loggedInUser = this.userService.getLoggedInUser();
-    }
-  
-    limpiarCampos() {
-      this.name = '';
-      this.lastname = '';
-      this.educationLevel = '';
-      this.isAnimated = true;
-
-      setTimeout(() => {
-        this.isAnimated = false;
-      }, 1000);
-    }
-
-    async mostrarDatos() {
-      const mensaje = `Nombre: ${this.name}  ${this.lastname}
+  async mostrarDatos() {
+    const mensaje = `Nombre: ${this.name}  ${this.lastname}
       || Nivel de educación: ${this.educationLevel}
-      || Fecha: ${this.fecha}`;	
+      || Fecha: ${this.fecha}
+      || Segmento seleccionado: ${this.segmentValue}`;
 
-      const alert = await this.alertController.create({
-        header: 'Datos Ingresados',
-        message: mensaje,
-        buttons: ['Cerrar']
-      });
-  
-      await alert.present();
-    }
+    const alert = await this.alertController.create({
+      header: 'Datos Ingresados',
+      message: mensaje,
+      buttons: ['Cerrar'],
+    });
+
+    await alert.present();
+  }
 }
